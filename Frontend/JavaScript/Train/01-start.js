abc632fac1c0fb7e1897cb0a26c0fead788079c0
Oscar Olsson <happybits@gmail.com>
2022-03-09 16:12:44 +0100

    -

---
+++ Frontend/JavaScript/Train/01-start.js
@@ -0,0 +1,28 @@
+const result = document.querySelector(".train .result")
+const from = document.querySelector(".train .from")
+const to = document.querySelector(".train .to")
+const okbutton = document.querySelector(".train button")
+let wayChoice = null
+
+update()
+
+// hint:
+// from.disabled = true // will disable the from-element
+
+function update() {
+    
+    /* insert code here */
+    
+}
+
+function wayChange(way) {
+
+    /* insert code here */
+
+}
+
+function makeChoice() {
+    
+    /* insert code here */
+
+}
